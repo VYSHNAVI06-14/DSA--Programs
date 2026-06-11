ecb24a045d33e70c7504ec1309fda517eb4a4d75
VyshnaviRamu <242ta05029@ashokacollege.in>
2026-06-11 12:44:11 +0530

    Create Secondmax.java

---
+++ Secondmax.java
@@ -0,0 +1,22 @@
+public class SecondMax {
+    public static void main(String[] args) 
+   {
+        int[] arr = {5, 2, 9, 1, 6};
+
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+
+        for (int x : arr)
+       {
+            if (x > first) 
+              {
+                second = first;
+                first = x;
+            } else if (x > second && x != first) {
+                second = x;
+            }
+        }
+
+        System.out.println("Second Largest = " + second);
+    }
+}
