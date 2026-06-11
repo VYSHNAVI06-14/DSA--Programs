ef26418bad4cd50e9a6a8ca7ca1a1f0070d0e47e
VyshnaviRamu <242ta05029@ashokacollege.in>
2026-06-11 12:39:08 +0530

    Create ZerotoEnd.java

---
+++ ZerotoEnd.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class ZeroToEnd {
+    public static void main(String[] args) {
+        int[] arr = {0, 2, 0, 5, 6, 0, 3};
+
+        int j = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+
+        System.out.println("Result = " + Arrays.toString(arr));
+    }
+}
