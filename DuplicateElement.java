4842728f4318fe2e6cf7f77410d25ae37f17e0e3
Vyshnavi <242ta05029@ashokacollege.in>
2026-08-15 18:51:10 +0530

    Program to remove duplicates

--- DuplicateElement.java
+++ DuplicateElement.java
@@ -1,17 +1,24 @@
 import java.util.HashSet;
 
 public class DuplicateElement
- {
+{
     public static void main(String[] args)
  {
         int[] arr = {1, 2, 3, 4, 2, 5, 3};
 
         HashSet<Integer> set = new HashSet<>();
-
+        HashSet<Integer> duplicates = new HashSet<>();
         for (int num : arr) {
             if (!set.add(num)) {
-                System.out.println("Duplicate: " + num);
+                duplicates.add(num);
             }
         }
+        for(int num:duplicates)
+        {
+            System.out.println("Duplicate: " + num);
+        }
     }
+
+        
+    
 }
